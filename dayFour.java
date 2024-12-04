bb05e7437d6d6fce212a720d436fdd30487d0f7f
RohanSen08 <rohanhenrysen@gmail.com>
2024-12-04 09:17:27 -0500

    more day four

---
+++ dayFour.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class dayFour {
+  public static void main(String[] args) throws FileNotFoundException {
+    System.out.println("Part one: ");
+    File file = new File("src/dayFour.txt");
+    Scanner input = new Scanner(file);
+    long sum = 0;
+    String[][] listOfChars = new String[][];
+    while (input.hasNextLine()) {
+
+      }
+      System.out.println(sum);
+  }
+}
